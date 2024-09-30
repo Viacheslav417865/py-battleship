9030079f7edf9144c076a6da1ebde84a73036754
Viacheslav417865 <sfinks417865@gmail.com>
2024-09-30 18:57:00 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-from typing import Any
+from typing import List, Tuple, Dict
 
 
 class Deck:
-    def __init__(self, row: Any, column: Any,
+    def __init__(self, row: int, column: int,
                  is_alive: bool = True) -> None:
         self.row = row
         self.column = column
@@ -10,13 +10,14 @@ class Deck:
 
 
 class Ship:
-    def __init__(self, start: Any, end: Any,
+    def __init__(self, start: Tuple[int, int], end: Tuple[int, int],
                  is_drowned: bool = False) -> None:
-        self.decks = []
+        self.decks: List[Deck] = []
         self.is_drowned = is_drowned
         self.create_decks(start, end)
 
-    def create_decks(self, start: Any, end: Any) -> None:
+    def create_decks(self, start: Tuple[int, int],
+                     end: Tuple[int, int]) -> None:
         start_row, start_col = start
         end_row, end_col = end
         if start_row == end_row:
@@ -26,13 +27,13 @@ class Ship:
             for row in range(start_row, end_row + 1):
                 self.decks.append(Deck(row, start_col))
 
-    def get_deck(self, row: Any, column: Any) -> Any:
+    def get_deck(self, row: int, column: int) -> Deck:
         for deck in self.decks:
             if deck.row == row and deck.column == column:
                 return deck
         return None
 
-    def fire(self, row: Any, column: Any) -> bool:
+    def fire(self, row: int, column: int) -> bool:
         deck = self.get_deck(row, column)
         if deck and deck.is_alive:
             deck.is_alive = False
@@ -43,16 +44,17 @@ class Ship:
 
 
 class Battleship:
-    def __init__(self, ships: object) -> None:
-        self.field = {}
-        self.ships = []
+    def __init__(self, ships: List[Tuple[Tuple[int, int], Tuple[int, int]]])\
+            -> None:
+        self.field: Dict[Tuple[int, int], Ship] = {}
+        self.ships: List[Ship] = []
         for start, end in ships:
             ship = Ship(start, end)
             self.ships.append(ship)
             for deck in ship.decks:
                 self.field[(deck.row, deck.column)] = ship
 
-    def fire(self, location: tuple) -> str:
+    def fire(self, location: Tuple[int, int]) -> str:
         row, col = location
         if (row, col) in self.field:
             ship = self.field[(row, col)]
